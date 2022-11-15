808e1f84e2d736b89412c8df5e47690be7646d7e
SilverFishSeeker <silverfishseeker@gmail.com>
2022-11-15 19:17:09 +0100

    Base del sistema de items. Items con ID persistentes y coherentes. Primer acercamiento al inventario.

---
+++ Assets/Scripts/ItemsMananger.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Linq;
+
+public class ItemsMananger : MonoBehaviour {
+
+    public static string RegPath;
+
+    private static IEnumerable<string> ReadIDs => File.ReadAllLines(RegPath);
+
+    private IEnumerable<string> ChildsIds() {
+        foreach (Transform child in transform)
+            yield return child.GetComponent<Item>().ID;
+    }
+    
+    static ItemsMananger() {
+        RegPath = GameStaticAccess.DataFolder+"registro";
+        if (!File.Exists(RegPath))
+            File.Create(RegPath).Dispose();
+    }
+
+    public bool EnsureItemIDsConsistency() {
+        HashSet<string> r = new HashSet<string>(ReadIDs);
+        foreach (string id in ChildsIds()) {
+            if (!r.Remove(id))
+                return false;
+        }
+
+        return r.Count == 0;
+    }
+
+    // Usar esta función cuando se ha eliminado items de la lista y
+    // se quieren eliminar sus ids del registro para así manterner la consistencia
+    public void ClearMissingIDsInRegister() {
+        File.WriteAllLines(RegPath, ReadIDs.Intersect(ChildsIds())); // ReadIDs - childsIds
+    }
+
+    public static bool AddNewID(string id) {
+        if (ReadIDs.Contains(id))
+            return false;
+        File.AppendAllText(RegPath, id + "\n");
+        return true;
+    }
+
+}
