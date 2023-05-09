e6f566141de29d299b415957d730daf51251abcc
SilverFishSeeker <silverfishseeker@gmail.com>
2023-05-09 11:26:03 +0200

    quitar múscia

--- Assets/Scripts/Radio.cs
+++ Assets/Scripts/Radio.cs
@@ -4,10 +4,25 @@ using UnityEngine;
 
 public class Radio : MonoBehaviour {
 
+    private Dictionary<AudioClip, AudioSource> onPlay;
+
+    void Start(){
+        onPlay = new Dictionary<AudioClip, AudioSource>();
+    }
+
     public void AddTrack(AudioClip clip){
             AudioSource asrc = gameObject.AddComponent<AudioSource>();
             asrc.clip = clip;
             asrc.loop = true;
             asrc.Play();
+            onPlay[clip] = asrc;
+    }
+
+    public bool RemoveTrack(AudioClip clip){
+        if (!onPlay.ContainsKey(clip))
+            return false;
+        Destroy(onPlay[clip]);
+        onPlay.Remove(clip);
+        return true;
     }
 }
